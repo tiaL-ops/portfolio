6609d5b110eb7675258ca37d5cec32cb616e9632
tiaL-ops <104815994+tiaL-ops@users.noreply.github.com>
2025-01-13 21:07:24 +0300

    Flipped

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 const grid = document.getElementById("myGrid");
 const item4 = document.getElementById("item4");
 const item2 = document.getElementById("item2");
-// Set the initial class
+
+
 grid.classList.add("initial");
 
 // Add a click event listener to item2
@@ -10,15 +11,17 @@ item2.addEventListener("click", () => {
   grid.classList.remove("initial");
   grid.classList.add("visible");
 });
+
+
 // Add a click event listener to item4
 item4.addEventListener("click", () => {
   console.log("item4 clicked");
 
   if (grid.classList.contains("big")) {
     console.log("Big, reverting to normal layout");
-    grid.classList.remove("big"); // Revert to normal layout
+    grid.classList.remove("big"); 
   } else {
     console.log("Expanding item4, shrinking others");
-    grid.classList.add("big"); // Expand item4 and shrink others
+    grid.classList.add("big"); 
   }
 });
